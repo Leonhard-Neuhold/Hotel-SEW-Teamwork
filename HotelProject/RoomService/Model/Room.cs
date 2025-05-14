d659861e1e4f6c27559c76ec7feeb7b2d9b0ee59
JacobToifl <j.toifl@htlkrems.at>
2025-05-14 14:54:01 +0200

    fixes

---
+++ HotelProject/RoomService/Model/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoomService.Model;
+
+[Table("ROOMS")]
+public class Room 
+{
+    [Column("ROOM_ID")]
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int RoomId { get; set; }
+    
+    [Column("NAME")]
+    [StringLength(3)]
+    [Required]
+    public string? RoomNumber { get; set; }
+}
