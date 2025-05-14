13ea7fcdaf563aa8d3545f3552559fd55b2050aa
Moritz Graschopf <moritz.graschopf@gmail.com>
2025-05-14 14:29:32 +0200

    Add customer feedback system with API endpoints
    Introduce the ability to submit and retrieve customer feedback. Implemented database schema, EF Core migrations, and services to handle feedback operations. Added RESTful API endpoints for complaint submission and feedback retrieval using controllers.

---
+++ HotelProject/KundenfeedbackService/Models/CustomerFeedback.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KundenfeedbackService.Models;
+
+public class CustomerFeedback
+{
+    [Key]
+    public int FeedbackId { get; set; }
+
+    [Required]
+    public int GuestId { get; set; }
+
+    [Required]
+    public string Complaint { get; set; } = string.Empty;
+
+    public DateTime SubmittedAt { get; set; } = DateTime.Now;
+}
--- HotelProject/KundenfeedbackService/Program.cs
+++ HotelProject/KundenfeedbackService/Program.cs
@@ -1,8 +1,19 @@
+using KundenfeedbackService.Config;
+using KundenFeedbackService.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FeedbackContext>(options =>
+    options.UseSqlite("Data Source=feedback.db"));
+
+builder.Services.AddScoped<CustomerFeedbackService>();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
